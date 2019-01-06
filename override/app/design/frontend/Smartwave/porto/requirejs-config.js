95619374e4362c6f7e3d45bea8b92855818e23a3
Timothy Keith <timothy@atvpc.com>
2019-01-06 18:55:46 +0000

    initical commit

---
+++ override/app/design/frontend/Smartwave/porto/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+	shim: {
+		jquery: { exports: '$' },
+		'owl.carousel/owl.carousel.min': { deps: ['jquery'] },
+		'js/jquery.stellar.min': { deps: ['jquery'] },
+		'js/jquery.parallax.min': { deps: ['jquery'] }
+	}
+};
+
