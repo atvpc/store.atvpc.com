ed11293184cc47379a6e70a05981c01f8402e8a5
Timothy Keith <timothy@atvpc.com>
2019-01-12 15:26:50 +0000

    fix requirejs errors

--- override/app/design/frontend/Smartwave/porto/requirejs-config.js
+++ override/app/design/frontend/Smartwave/porto/requirejs-config.js
@@ -1,6 +1,7 @@
 var config = {
 	shim: {
 		jquery: { exports: '$' },
+		'Smartwave_Megamenu/js/sw_megamenu': { deps: ['jquery'] },
 		'owl.carousel/owl.carousel.min': { deps: ['jquery'] },
 		'js/jquery.stellar.min': { deps: ['jquery'] },
 		'js/jquery.parallax.min': { deps: ['jquery'] }
