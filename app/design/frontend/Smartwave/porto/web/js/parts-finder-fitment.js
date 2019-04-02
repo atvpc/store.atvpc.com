2547ce95e9987fc3408dbf9bb613088ab56c2e2c
Timothy Keith <timothykeith@gmail.com>
2019-04-02 17:38:00 -0400

    fixed element selector

--- app/design/frontend/Smartwave/porto/web/js/parts-finder-fitment.js
+++ app/design/frontend/Smartwave/porto/web/js/parts-finder-fitment.js
@@ -2,26 +2,33 @@ require(['jquery'], function( $ ) {
   "use strict";
 
 
-	var yearElem = document.getElementsByName('finder[1]')[0];
-	var modelElem = document.getElementsByName('finder[3]')[0];
-	var submodelElem = document.getElementsByName('finder[4]')[0];
-	
-	if ( submodelElem.selectedIndex !== -1) {
-		var year = yearElem.options[ yearElem.selectedIndex ].text; 
-		var make = document.querySelector('[data-amfinder-js="text"]').innerHTML;
-		var model = modelElem.options[ modelElem.selectedIndex ].text; 
-		var submodel = submodelElem.options[ submodelElem.selectedIndex ].text; 
+	function update_fitments() {
+		var year     = $('#finder-1--1 option:selected').text().trim();
+		var make     = $('#finder-1--2 span[data-amfinder-js="text"]').html();
+		var model    = $('#finder-1--3 option:selected').text().trim();
+		var submodel = $('#finder-1--4 option:selected').text().trim();
 
-		$.getJSON('/fitments/index.php?' +
-					'year=' + year + 
-					'&make=' + make + 
-					'&model=' + model +
-					'&submodel=' + submodel, function(data) {
+		console.log("Year: " + year);
+		console.log("Make: " + make);
+		console.log("Model: " + model);
+		console.log("Submodel: " + submodel);
 
-			$.each(result, function(i, item) {
-				$(".fitment[data-sku='" + item.SKU + "']").html( item.FIT );
+		$.getJSON('/fitment/index.php?year=' + year + '&make=' + make + '&model=' + model + '&submodel=' + submodel, function(data) {
+			console.log(data);
+
+			$.each(data, function(i, item) {
+				$(".fitloc[data-sku='" + item.SKU + "']").html( item.LOC );
 			});
+		}).fail(function(data, textStatus, error){
+        		console.error("fitment getJSON failed, status: " + textStatus + ", error: "+error)
 		});
 	}
 
+	$('document').ready(function(){
+		if ($("#finder-1--4 option:selected").val() !== 0) {
+			setTimeout(function(){
+				update_fitments();
+			}, 2000);
+		}
+	});
 });
