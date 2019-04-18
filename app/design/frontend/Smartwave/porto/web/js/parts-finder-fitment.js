073e22eb4b448daf0536192f1d3932a6af5dcc55
Timothy Keith <timothy@atvpc.com>
2019-04-18 17:52:35 +0000

    don't try to parse vehicle until all fitments have been set

--- app/design/frontend/Smartwave/porto/web/js/parts-finder-fitment.js
+++ app/design/frontend/Smartwave/porto/web/js/parts-finder-fitment.js
@@ -4,24 +4,28 @@ require(['jquery'], function( $ ) {
 
 	function update_fitments() {
 		var year     = $('#finder-1--1 option:selected').text().trim();
-		var make     = $('#finder-1--2 span[data-amfinder-js="text"]').html();
+		var make     = $('#finder-1--2 span[data-amfinder-js="text"]').html().trim();
 		var model    = $('#finder-1--3 option:selected').text().trim();
 		var submodel = $('#finder-1--4 option:selected').text().trim();
 
-		console.log("Year: " + year);
-		console.log("Make: " + make);
-		console.log("Model: " + model);
-		console.log("Submodel: " + submodel);
+		var emptyText = "Please Select ...";
+		if (year != emptyText && make != emptyText && model != emptyText && submodel != emptyText) {
+			console.log("Year: " + year);
+			console.log("Make: " + make);
+			console.log("Model: " + model);
+			console.log("Submodel: " + submodel);
 
-		$.getJSON('/fitment/index.php?year=' + year + '&make=' + make + '&model=' + model + '&submodel=' + submodel, function(data) {
-			console.log(data);
 
-			$.each(data, function(i, item) {
-				$(".fitloc[data-sku='" + item.SKU + "']").html( item.LOC );
+			$.getJSON('/fitment/index.php?year=' + year + '&make=' + make + '&model=' + model + '&submodel=' + submodel, function(data) {
+				console.log(data);
+
+				$.each(data, function(i, item) {
+					$(".fitloc[data-sku='" + item.SKU + "']").html( item.LOC );
+				});
+			}).fail(function(data, textStatus, error){
+        			console.error("fitment getJSON failed, status: " + textStatus + ", error: "+error)
 			});
-		}).fail(function(data, textStatus, error){
-        		console.error("fitment getJSON failed, status: " + textStatus + ", error: "+error)
-		});
+		}
 	}
 
 	$('document').ready(function(){
