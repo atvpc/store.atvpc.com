42d352e347a0723eb15f543d5feae658f82e9606
Timothy Keith <timothykeith@gmail.com>
2019-08-14 14:19:13 +0000

    fixed issue with checkouts breaking due to fitment locator plugin's JS

--- app/design/frontend/Smartwave/porto/web/js/parts-finder-fitment.js
+++ app/design/frontend/Smartwave/porto/web/js/parts-finder-fitment.js
@@ -3,28 +3,34 @@ require(['jquery'], function( $ ) {
 
 
 	function update_fitments() {
-		var year     = $('#finder-1--1 option:selected').text().trim();
-		var make     = $('#finder-1--2 span[data-amfinder-js="text"]').html().trim();
-		var model    = $('#finder-1--3 option:selected').text().trim();
-		var submodel = $('#finder-1--4 option:selected').text().trim();
+		if ( $(document).find("#amfinder_1").length !== 0 ) {
+			var year     = $('#finder-1--1 option:selected').text().trim();
+			var make     = $('#finder-1--2 span[data-amfinder-js="text"]').html().trim();
+			var model    = $('#finder-1--3 option:selected').text().trim();
+			var submodel = $('#finder-1--4 option:selected').text().trim();
 
-		var emptyText = "Please Select ...";
-		if (year != emptyText && make != emptyText && model != emptyText && submodel != emptyText) {
-			console.log("Year: " + year);
-			console.log("Make: " + make);
-			console.log("Model: " + model);
-			console.log("Submodel: " + submodel);
+			var emptyText = "Please Select ...";
+
+			if (year != emptyText && make != emptyText && model != emptyText && submodel != emptyText) {
+				console.log("Year: " + year);
+				console.log("Make: " + make);
+				console.log("Model: " + model);
+				console.log("Submodel: " + submodel);
 
 
-			$.getJSON('/fitment/index.php?year=' + year + '&make=' + make + '&model=' + model + '&submodel=' + submodel, function(data) {
-				console.log(data);
+				$.getJSON('/fitment/index.php?year=' + year + '&make=' + make + '&model=' + model + '&submodel=' + submodel, function(data) {
+					console.log(data);
 
-				$.each(data, function(i, item) {
-					$(".fitloc[data-sku='" + item.SKU + "']").html( item.LOC );
+					$.each(data, function(i, item) {
+						$(".fitloc[data-sku='" + item.SKU + "']").html( item.LOC );
+					});
+				}).fail(function(data, textStatus, error){
+        				console.error("fitment getJSON failed, status: " + textStatus + ", error: "+error)
 				});
-			}).fail(function(data, textStatus, error){
-        			console.error("fitment getJSON failed, status: " + textStatus + ", error: "+error)
-			});
+			}
+		}
+		else {
+			console.log("amasty fitment finder plugin not present on page, disabling ATVPC update_fitments()");
 		}
 	}
 
